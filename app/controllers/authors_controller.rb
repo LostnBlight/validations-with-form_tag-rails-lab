39e4ddd4ca58076044ea66f4ba506c2579c67773
Your Name <you@example.com>
2021-04-27 19:42:58 -0600

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,7 @@
 class AuthorsController < ApplicationController
+
+    before_action :set_author, only: [:show, :edit, :update]
+
   def show
     @author = Author.find(params[:id])
   end
@@ -10,10 +13,29 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create(author_params)
 
-    redirect_to author_path(@author)
+    if @author.valid?
+        @author.save
+        redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
+  def update
+    @author.update(author_params)
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
+  end
+
+
   private
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
 
   def author_params
     params.permit(:name, :email, :phone_number)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+
+    before_action :set_post, only: [:show, :edit, :update]
+
   def show
     @post = Post.find(params[:id])
   end
@@ -7,15 +10,32 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def create
+    @post = Post.create(post_params)
+
+    if @post.valid?
+        @post.save
+        redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
   def update
-    @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
+   @post.update(post_params)
+   if @post.valid?
     redirect_to post_path(@post)
+   else
+    render :edit
+   end
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 
   def post_params
     params.permit(:title, :category, :content)
